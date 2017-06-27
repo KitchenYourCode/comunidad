29968df1abcf58d70d5f916e2f18fcab592ecca4
Wanda-PC <elias.saavedra.lol@gmail.com>
2017-06-27 11:58:16 -0300

    Agregado mini navegador

--- src/Indexs/Home/Home.js
+++ src/Indexs/Home/Home.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import Header from '../../components/Header/Header.js';
 import LogIn from '../../components/LogIn/LogIn.js';
+import {
 
+  Link
+} from 'react-router-dom';
 export default class Home extends Component{
   render(){
     return(
       <div>
       <Header/>
+      <ul>
+     <li><Link to="/">Home</Link></li>
+     <li><Link to="/Dashboard">Dashboard</Link></li>
+     <li><Link to="/CrearUsuarios">CrearUsuarios</Link></li>
+     <li><Link to="/CrearPoligonos">CrearPoligonos</Link></li>
+   </ul>
       <LogIn />
     </div>
     );
